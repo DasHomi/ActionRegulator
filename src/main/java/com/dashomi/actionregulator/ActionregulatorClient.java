bf4a5f1c6839644e7d6f05f4917ca46755c457fd
DasHomi <72689274+DasHomi@users.noreply.github.com>
2026-02-06 20:43:00 +0100

    Initial commit

---
+++ src/main/java/com/dashomi/actionregulator/ActionregulatorClient.java
@@ -0,0 +1,10 @@
+package com.dashomi.actionregulator;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ActionregulatorClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
